3674ef62fa41e04087d8cbace47211d80b5613d6
Suman Adhikari <expertxkid@gmail.com>
2025-07-25 08:05:59 -0500

    feat: add exercise solution

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello, GraphQL Basics!");
+import myCurrentLocation, { message, name, getGreeting } from "./myModule";
+import addition, { subtract } from "./math";
+console.log(message);
+console.log(name);
+
+console.log(myCurrentLocation);
+
+console.log(getGreeting(name));
+
+console.log(addition(5, 3));
+console.log(subtract(10, 4));
