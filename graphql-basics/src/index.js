b10fc1cf50dee438df9a6f280844f8bb5cb2b80e
Suman Adhikari <expertxkid@gmail.com>
2025-07-26 21:34:54 -0500

    feat: add bi-directional relationship between Comment and User

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -61,13 +61,42 @@ const posts = [
   },
 ];
 
+const comments = [
+  {
+    id: "1",
+    text: "Great post!",
+    author: "1",
+  },
+  {
+    id: "2",
+    text: "Very informative.",
+    author: "2",
+  },
+  {
+    id: "3",
+    text: "I learned a lot from this.",
+    author: "3",
+  },
+  {
+    id: "4",
+    text: "Can't wait to apply this knowledge!",
+    author: "1",
+  },
+  {
+    id: "5",
+    text: "Thanks for sharing!",
+    author: "2",
+  },
+];
+
 // type definitions (schema)
 const typeDefs = `
     type Query {
         users(query: String): [User!]!
         posts(query:String): [Post!]!
         me: User!
-        post: Post!        
+        post: Post!      
+        comments: [Comment!]!  
     }
 
     type User {
@@ -75,7 +104,8 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int  
-        posts: [Post!]!      
+        posts: [Post!]!    
+        comments: [Comment!]!  
     }
 
     type Post {
@@ -83,6 +113,12 @@ const typeDefs = `
         title: String!
         body: String!
         published: Boolean!
+        author: User!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
         author: User!
     }
 `;
@@ -126,6 +162,9 @@ const resolvers = {
         published: true,
       };
     },
+    comments() {
+      return comments;
+    },
   },
   Post: {
     author(parent, args, ctx, info) {
@@ -135,6 +174,14 @@ const resolvers = {
   User: {
     posts(parent, args, ctx, info) {
       return posts.filter((post) => post.author === parent.id);
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => comment.author === parent.id);
+    },
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => user.id === parent.author);
     },
   },
 };
