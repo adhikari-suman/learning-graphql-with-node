83349fffeb4c7c7c45d079e012a1c908c44d6762
Suman Adhikari <expertxkid@gmail.com>
2025-07-28 07:36:37 -0500

    refactor: add deleteComment resolver and mutation

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -111,6 +111,7 @@ const typeDefs = `
       createPost(data: CreatePostInput): Post!
       deletePost(id: ID!): Post!
       createComment(data: CreateCommentInput): Comment!
+      deleteComment(id: ID!): Comment!
     }
 
     input CreateUserInput {
@@ -284,6 +285,19 @@ const resolvers = {
       comments.push(comment);
 
       return comment;
+    },
+    deleteComment(parent, args, ctx, info) {
+      const commentIdx = comments.findIndex(
+        (comment) => comment.id === args.id
+      );
+
+      if (commentIdx === -1) {
+        throw new Error("Comment not found.");
+      }
+
+      const deletedComments = comments.splice(commentIdx, 1);
+
+      return deletedComments[0];
     },
   },
   Post: {
