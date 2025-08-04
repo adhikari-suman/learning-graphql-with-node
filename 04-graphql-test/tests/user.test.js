6bca78dd2366ba46810c119b38699237ab8323cf
Suman Adhikari <expertxkid@gmail.com>
2025-08-04 10:55:13 -0500

    chore: add dummy tests for practice

--- 04-graphql-test/tests/user.test.js
+++ 04-graphql-test/tests/user.test.js
@@ -1,5 +1,56 @@
-test("This is my first test case", () => {
-  1 + 2;
+import { getFirstName, isValidPassword } from "../src/utils/user.js";
+
+test("should return first name when given full name", () => {
+  // arrange
+  const fullName = "Suman Adhikari";
+
+  // act
+  const firstName = getFirstName(fullName);
+
+  // assert
+  expect(firstName).toBe("Suman");
 });
 
-test("This is my second test case", () => {});
+test("should return first name when given first name", () => {
+  // arrange
+  const fullName = "Suman";
+
+  // act
+  const firstName = getFirstName(fullName);
+
+  // assert
+  expect(firstName).toBe("Suman");
+});
+
+test("should reject password shorter than 8 characters", () => {
+  // arrange
+  const password = "Suman";
+
+  // act
+  const isValid = isValidPassword(password);
+
+  // assert
+  expect(isValid).toBe(false);
+});
+
+test("should reject password that contains the word password", () => {
+  // arrange
+  const password = "password1234";
+
+  // act
+  const isValid = isValidPassword(password);
+
+  // assert
+  expect(isValid).toBe(false);
+});
+
+test("should correctly validate a password", () => {
+  // arrange
+  const password = "Testing@123";
+
+  // act
+  const isValid = isValidPassword(password);
+
+  // assert
+  expect(isValid).toBe(true);
+});
